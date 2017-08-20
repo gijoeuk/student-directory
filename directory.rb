bbe800c9574381d6858f49aaad4b9c9e41623a48
gijoeuk <josephcingram@hotmail.co.uk>
2017-08-20 14:00:54 +0100

    Add changes for user input method

--- directory.rb
+++ directory.rb
@@ -1,32 +1,45 @@
 # First we print the list of students
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+# students = [
+#   {name: "Dr. Hannibal Lecter", cohort: :november},
+#   {name: "Darth Vader", cohort: :november},
+#   {name: "Nurse Ratched", cohort: :november},
+#   {name: "Michael Corleone", cohort: :november},
+#   {name: "Alex DeLarge", cohort: :november},
+#   {name: "The Wicked Witch of the West", cohort: :november},
+#   {name: "Terminator", cohort: :november},
+#   {name: "Freddy Krueger", cohort: :november},
+#   {name: "The Joker", cohort: :november},
+#   {name: "Joffrey Baratheon", cohort: :november},
+#   {name: "Norman Bates", cohort: :november}
+# ]
+
 def print_header
-puts "The students of Villains Academy"
-puts "-----------"
+  puts "The students of Villains Academy"
+  puts "-----------"
 end
 
 def print_names(students)
-students.each {|student|puts "#{student[:name]} (#{student[:cohort]} cohort)"}
+  students.each {|student|puts "#{student[:name]} (#{student[:cohort]} cohort)"}
 end
 
-# Finally, we print the number of students
 def print_footer(students)
-puts "Overall we have #{students.count} great students"
+  puts "Overall we have #{students.count} great students"
 end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+    while !name.empty?
+      students << {name: name, cohort: :november}
+      puts "Now we have #{students.count} students"
+      name = gets.chomp
+    end
+  students
+end
+
+students = input_students
 print_header
 print_names(students)
 print_footer(students)
-
-#Its important that print doesnt add new line characters
