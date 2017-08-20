3632f6c931bfce1908fcf2823631e741e2d191a1
gijoeuk <josephcingram@hotmail.co.uk>
2017-08-20 14:59:06 +0100

    Add interactive menu to student-directory

--- directory.rb
+++ directory.rb
@@ -14,8 +14,8 @@
 # ]
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-----------"
+    puts "The students of Villains Academy"
+    puts "-----------"
 end
 
 def print_names(students)
@@ -23,7 +23,7 @@ def print_names(students)
 end
 
 def print_footer(students)
-  puts "Overall we have #{students.count} great students"
+    puts "Overall we have #{students.count} great students"
 end
 
 def input_students
@@ -39,7 +39,31 @@ def input_students
   students
 end
 
-students = input_students
-print_header
-print_names(students)
-print_footer(students)
+
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "What do you want to do"
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+   # 2. read the input and save it into a variable
+   selection = gets.chomp
+   # 3. do what the user has asked
+   case selection
+     when "1"
+       students = input_students
+     when "2"
+       print_header
+       print_names(students)
+       print_footer(students)
+     when "9"
+       exit #
+     else
+       puts "I don't know what you meant, try again"
+   end
+ end
+end
+
+interactive_menu
